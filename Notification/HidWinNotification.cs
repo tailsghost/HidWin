61d57917815db06514f90929fec76926be376917
AndreyXP <andreyapple99@mail.ru>
2026-06-18 13:04:13 +0300

    update

--- Notification/HidWinNotification.cs
+++ Notification/HidWinNotification.cs
@@ -2,25 +2,25 @@
 
 namespace HidWin.Notification;
 
-public static class HidWinNotification
+public class HidWinNotification
 {
 
-    public static event Action Connect;
-    public static event Action Disconnect;
+    public event Action Connect;
+    public event Action Disconnect;
 
-    public static void AutoConnect(int vid, int pid, DevineInterface deviceInfo)
+    public void AutoConnect(int vid, int pid, DevineInterface deviceInfo)
     {
         var listener = new DeviceNotificationListener(new TargetVidPid(vid, pid), deviceInfo);
         listener.DeviceMatchedConnected += Start;
         listener.DeviceMatchedDisconnected += Stop;
     }
 
-    private static void Start()
+    private void Start()
     {
         Connect?.Invoke();
     }
 
-    private static void Stop()
+    private void Stop()
     {
         Disconnect?.Invoke();
     }
